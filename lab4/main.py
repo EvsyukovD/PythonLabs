d8206fd74fc975a86f6cd6e2ac67192ce62475c4
Gibson <70591237+EvsyukovD@users.noreply.github.com>
2022-04-16 10:22:56 +0300

    Add files via upload

---
+++ lab4/main.py
@@ -0,0 +1,15 @@
+import managermodule as manager
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='The name of file with text')
+    parser.add_argument('-p', help='The name of file with patterns (1 pattern on each line)')
+    parser.add_argument('-l', help='The len of one pattern')
+    args = parser.parse_args()
+    manager.work(args.f, args.p, int(args.l))
+
+
+if __name__ == '__main__':
+    main()
