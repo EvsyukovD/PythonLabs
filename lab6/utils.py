cb341dd0bcbefe1798bf63a3fb22e707dfd35883
Gibson <70591237+EvsyukovD@users.noreply.github.com>
2022-05-14 12:16:59 +0300

    Delete utils.py

--- lab6/utils.py
+++
@@ -1,8 +0,0 @@
-def check(n):
-    if n == 1 or n < 0 or n % 2 == 1:
-        return False
-    while n % 2 == 0:
-        n = n // 2
-    if n % 2 == 1 and n != 1:
-        return False
-    return True
